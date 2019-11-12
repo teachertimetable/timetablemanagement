32e2544cca286f5f4086ebd4dc384a9f00781864
Asanan Aphisitworachorch <thaixperiasp@gmail.com>
2019-11-12 13:37:51 +0700

    make timetable table for inserting an JSONed table
    Signed-off-by: Asanan Aphisitworachorch <thaixperiasp@gmail.com>

---
+++ database/migrations/2019_11_12_062057_create_timetable_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTimetableTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('timetable_main', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('timetable_name');
+            $table->json('timetable_info');
+            $table->foreign('user_id')
+                ->references ('id')->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('timetable_main');
+    }
+}
