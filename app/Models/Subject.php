b566cf1b4e2689110c4bc863d211858a845f1de5
Asanan Aphisitworachorch <thaixperiasp@gmail.com>
2019-11-13 14:50:55 +0700

    Add Fillables , Primary Key , Incrementing Set To False
    Signed-off-by: Asanan Aphisitworachorch <thaixperiasp@gmail.com>

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -6,6 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Subject extends Model
 {
+    protected $table = 'subject';
+    protected $primaryKey = 'subject_id';
+    public $incrementing = false;
+
+    protected $fillable = [
+        'subject_id','subject_name','credit','teacher_id','start_time','end_time'
+    ];
+
     public function have(){
         return $this->hasMany ('App\Models\TeacherInfo');
     }
--- app/Models/TeacherInfo.php
+++ app/Models/TeacherInfo.php
@@ -6,6 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class TeacherInfo extends Model
 {
+    protected $table = 'teacher_info';
+    protected $primaryKey = 'teacher_id';
+    public $incrementing = false;
+
+    protected $fillable = [
+        'teacher_id','teacher_name','teacher_pic_src','teacher_email','teacher_tel'
+    ];
+
     public function teach(){
         return $this->hasMany ('App\Models\Subject');
     }
