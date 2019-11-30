985b08382dee0169764f03a47130141a359ae8e0
Asanan Aphisitworachorch <thaixperiasp@gmail.com>
2019-11-30 14:03:55 +0700

    Goodbye Roles ! You're ...
    Signed-off-by: Asanan Aphisitworachorch <thaixperiasp@gmail.com>

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('welcomesite')->with('title','ยินดีต้อนรับ');
+        return view ( 'welcomesite' );
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([
-            TeacherInfoSeeder::class,
-        ]);
+        $this->call ( PermissionsTableSeeder::class );
     }
 }
