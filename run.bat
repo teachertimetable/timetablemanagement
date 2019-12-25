3144f48ed48813e46888b776222933451535ee35
Asanan Aphisitworachorch <thaixperiasp@gmail.com>
2019-12-25 16:28:00 +0700

    Welcome To New World of HOSTING
    Signed-off-by: Asanan Aphisitworachorch <thaixperiasp@gmail.com>

--- run.bat
+++ run.bat
@@ -1,1 +1,15 @@
-php artisan serve
+@echo off
+
+echo    ===========================================================
+echo     TTTTTTT BBBBB   MM    MM   GGGG  MM    MM TTTTTTT  SSSSS
+echo       TTT   BB   B  MMM  MMM  GG  GG MMM  MMM   TTT   SS
+echo       TTT   BBBBBB  MM MM MM GG      MM MM MM   TTT    SSSSS
+echo       TTT   BB   BB MM    MM GG   GG MM    MM   TTT        SS
+echo       TTT   BBBBBB  MM    MM  GGGGGG MM    MM   TTT    SSSSS
+echo    ===========================================================
+echo            TIMETABLE MANAGEMENT SYSTEM FOR DIGITECH SUT
+echo    ===========================================================
+echo                        DEBUGGING SESSION
+echo    ===========================================================
+
+php artisan serve --host=0.0.0.0 --port=80
